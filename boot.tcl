5fcc70ae05a7d229c94d13f2094a1798fbbeee3f
aspect <aspect>
2015-11-26 09:38:51 +0000

    improve bootstrap

--- boot.tcl
+++ boot.tcl
@@ -2,4 +2,15 @@
 #
 lappend auto_path [file normalize [info script]/../modules]
 ::tcl::tm::path add [file normalize [info script]/../modules]
-source [lindex $argv 0][set argv [lrange $argv 1 end]]
+
+if {$::argv ne ""} {
+    proc info_cmdline {} [list list [info nameofexe] $::argv0 $::argv]      ;# hack for restartability
+    set ::argv [lassign $::argv ::argv0]
+    source $::argv0
+} else {
+    # async repl:
+    package require repl
+    coroutine main repl::chan stdin stdout stderr
+    trace add command main delete {unset ::forever; #}
+    vwait forever
+}
