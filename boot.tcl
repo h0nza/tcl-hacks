81fd57a5663e76d56c820361c0aef42fe8950bd3
aspect <aspect>
2016-08-07 03:13:51 +0000

    boot with a proc

--- boot.tcl
+++ boot.tcl
@@ -1,7 +1,13 @@
 #!/usr/bin/env tclsh
 #
-lappend auto_path [file normalize [info script]/../modules]
-::tcl::tm::path add [file normalize [info script]/../modules]
+#lappend auto_path [file normalize [info script]/../modules]
+#::tcl::tm::path add [file normalize [info script]/../modules]
+proc boot {args} [format {
+    {*}$args [list lappend auto_path %1$s]
+    {*}$args [list ::tcl::tm::path add %1$s]
+} [list [file normalize [info script]/../modules]]]
+boot eval
+package provide boot 0.1
 
 if {$::argv ne ""} {
     proc info_cmdline {} [list list [info nameofexe] $::argv0 $::argv]      ;# hack for restartability
